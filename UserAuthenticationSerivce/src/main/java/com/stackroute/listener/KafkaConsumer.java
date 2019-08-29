a81e3b88c157282dca0bca061e1764fc4506c4ca
rohitchakravorty <rohit.rou@gmail.com>
2019-08-29 14:50:32 +0530

    Simultaneous operation across services

--- UserAuthenticationSerivce/src/main/java/com/stackroute/listener/KafkaConsumer.java
+++ UserAuthenticationSerivce/src/main/java/com/stackroute/listener/KafkaConsumer.java
@@ -1,11 +1,10 @@
 package com.stackroute.listener;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import com.stackroute.dao.UserDao;
 import com.stackroute.model.Users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,14 +19,20 @@ public class KafkaConsumer {
     @Autowired
     private PasswordEncoder bcryptEncoder;
     @KafkaListener(topics = "registration", groupId="group_id")
-   public Users consumeJson(Users user) throws IOException {
+   public void consumeJson(Users user) throws IOException {
 //        User user = new ObjectMapper().readValue(message, Users.class);
         System.out.println("Consumed JSON Message: " + user);
         Users newUser = new Users();
         newUser.setEmail(user.getEmail());
         newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
         newUser.setRole(user.getRole());
-        return userDao.save(newUser);
+        if(user.getOperation()==1) {
+            userDao.save(newUser);
+        }
+        else if(user.getOperation()==0)
+        {
+            userDao.deleteByEmail(user.getEmail());
+        }
 
     }
 }
